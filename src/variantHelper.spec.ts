cf92cfa3714f07bf17474a1c8b3246b00c832584
Viktor Leandersson <viktor.leandersson@tractable.ai>
2020-06-07 18:54:43 +0100

    Adds tests for variantHelper

---
+++ src/variantHelper.spec.ts
@@ -0,0 +1,19 @@
+import { variantHelper } from "./variantHelper";
+
+const variants = variantHelper({
+  size: { small: "test_small", medium: "test_medium", large: "test_large" },
+});
+
+test("Mixin should be of type function", () => {
+  expect(typeof variants).toBe("function");
+});
+
+test("Selecting variants should return its content", () => {
+  const smallVariant = variants({ size: "small", theme: {} });
+  const mediumVariant = variants({ size: "medium", theme: {} });
+  const largeVariant = variants({ size: "large", theme: {} });
+
+  expect(smallVariant[0]).toBe("test_small");
+  expect(mediumVariant[0]).toBe("test_medium");
+  expect(largeVariant[0]).toBe("test_large");
+});
