af9d50269de60db72ff741cd05f986f657c60753
Viktor Leandersson <viktor.leandersson@tractable.ai>
2020-06-07 19:03:55 +0100

    Setsup webpack base

--- webpack.config.js
+++ webpack.config.js
@@ -18,5 +18,8 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd",
+    library: "simple-variant-helper",
+    umdNamedDefine: true,
   },
 };
